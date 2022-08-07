adb8081d91ea4e781ef11a8a7542fbd574cfca6c
MyseriousKnight <63143500+AeneonLXC@users.noreply.github.com>
2022-08-07 11:15:03 +0800

    Create index.py

---
+++ index.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  7 11:14:01 2022
+
+@TwinkelStar: 一直闪闪发光的李星辰
+@Email: xingchenziyi@163.com   
+ 
+# ----------------------------------------------
+#
+#           基于深度学习的人脸手势考勤系统
+#               Coding By lxc
+#                  模块name
+#
+#          LAST_UPDATE: Sun Aug  7 11:14:01 2022
+#
+# ----------------------------------------------
+
+"""
+import PyQt5 as qt
